b3f946463031589a205842ab017ac06e19c057ea
Evan Walter Clark Spotte-Smith, PhD <espottesmith@gmail.com>
2025-07-25 10:16:57 -0400

    More transform stuff

--- src/core/transform.jl
+++ src/core/transform.jl
@@ -576,11 +576,75 @@ function MLUtils.batch(hgs::AbstractVector{HGNNDiHypergraph{T,D}}) where {T <: R
     combine_hypergraphs(hgs)
 end
 
-# TODO: you are here
-function get_hypergraph()
+if g.graph_indicator === nothing
+    @assert i == [1]
+    if nmap
+        return g, 1:(g.num_nodes)
+    else
+        return g
+    end
 end
 
-function MLUtils.unbatch()
+node_mask = g.graph_indicator .∈ Ref(i)
+
+nodes = (1:(g.num_nodes))[node_mask]
+nodemap = Dict(v => vnew for (vnew, v) in enumerate(nodes))
+
+graphmap = Dict(i => inew for (inew, i) in enumerate(i))
+graph_indicator = [graphmap[i] for i in g.graph_indicator[node_mask]]
+
+s, t = edge_index(g)
+w = get_edge_weight(g)
+edge_mask = s .∈ Ref(nodes)
+
+if g.graph isa COO_T
+    s = [nodemap[i] for i in s[edge_mask]]
+    t = [nodemap[i] for i in t[edge_mask]]
+    w = isnothing(w) ? nothing : w[edge_mask]
+    graph = (s, t, w)
+elseif g.graph isa ADJMAT_T
+    graph = g.graph[nodes, nodes]
+end
+
+ndata = getobs(g.ndata, node_mask)
+edata = getobs(g.edata, edge_mask)
+gdata = getobs(g.gdata, i)
+
+num_edges = sum(edge_mask)
+num_nodes = length(graph_indicator)
+num_graphs = length(i)
+
+gnew = GNNGraph(graph,
+                num_nodes, num_edges, num_graphs,
+                graph_indicator,
+                ndata, edata, gdata)
+
+if nmap
+    return gnew, nodes
+else
+    return gnew
+end
+
+# TODO: you are here
+function get_hypergraph(hg::HGNNHypergraph, i::Int; map_vertices::Bool=false)
+    if hg.hypergraph_ids === nothing
+        @assert i == 1
+
+        if map_vertices
+            return hg, 1:(hg.num_vertices)
+        else
+            return hg
+        end
+    end
+
+end
+
+function get_hypergraph(hg::HGNNHypergraph, i::AbstractVector{Int})
+
+end
+
+function MLUtils.unbatch(hg::HGNNHypergraph)
+    return [get_hypergraph(hg, i) for i in 1:(hg.num_hypergraphs)]
 end
 
 function negative_sample()
@@ -590,6 +654,9 @@ function random_split_vertices()
 end
 
 function random_split_hyperedges()
+end
+
+function random_split_hypergraphs()
 end
 
 # TODO: PageRank diffusion?
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
 using HyperGraphNeuralNetworks
 using Test
-using JET
+# using JET
 
-@testset "HyperGraphNeuralNetworks.jl" begin
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(HyperGraphNeuralNetworks; target_defined_modules = true)
-    end
-    # Write your tests here.
-end
+# @testset "HyperGraphNeuralNetworks.jl" begin
+#     @testset "Code linting (JET.jl)" begin
+#         JET.test_package(HyperGraphNeuralNetworks; target_defined_modules = true)
+#     end
+#     # Write your tests here.
+# end
